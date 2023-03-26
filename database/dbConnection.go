d75a942023e2fe84ad9c409c00860f7bba34daff
Ojas Thakur <ojasthakur1999@gmail.com>
2023-03-26 19:08:34 +0530

    loading .env in main.go file

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -7,17 +7,11 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	mongoURI := os.Getenv("MONGO_URI")
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	routes "jwt-auth-gin-gonic/routes"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load the .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	// read from env var
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
